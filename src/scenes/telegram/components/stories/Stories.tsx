f3be26a570be9017db5f3c9e2f8ade4ceb8342cf
Alessio <alessio.marinoni1@gmail.com>
2025-12-06 00:32:31 +0100

    removed unused stories mock

--- src/scenes/telegram/components/stories/Stories.tsx
+++ src/scenes/telegram/components/stories/Stories.tsx
@@ -1,19 +1,6 @@
 import { useState } from "react";
 import "./Stories.scss";
 
-const storiesData = [
-  { id: 1, image: "/assets/sample.png" },
-  { id: 2, image: "/assets/sample.png" },
-  { id: 3, image: "/assets/sample.png" },
-  { id: 4, image: "/assets/sample.png" },
-  { id: 5, image: "/assets/sample.png" },
-  { id: 6, image: "/assets/sample.png" },
-  { id: 7, image: "/assets/sample.png" },
-  { id: 8, image: "/assets/sample.png" },
-  { id: 9, image: "/assets/sample.png" },
-  { id: 10, image: "/assets/sample.png" },
-];
-
 const getUserImg = (user: string) => {
   return `/assets/${user}.jpg`;
 };
